19c608330590766a5b4dd6fdf3cd91b7bd4a550a
BaldurNavarro <navarrobaldur@gmail.com>
2025-08-12 06:15:21 -0600

    desactivacion optimizacion next image

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["localhost", "127.0.0.1", "ehtwuxuwinsoyrsusuyu.supabase.co", "placehold.co", "image.tmdb.org"],
+    domains: [
+      "localhost",
+      "127.0.0.1",
+      "ehtwuxuwinsoyrsusuyu.supabase.co",
+      "placehold.co",
+      "image.tmdb.org",
+    ],
+    unoptimized: true, // 👈 Esto desactiva la optimización de imágenes
   },
 };
 
